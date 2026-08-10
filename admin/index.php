f040a46f5c36aa2a49297f40d7ebee24b054bbce
ambe7888 <ambe.akponi@uvci.edu.ci>
2026-08-10 14:53:41 +0000

    Fix CSS caching issue

--- admin/index.php
+++ admin/index.php
@@ -26,7 +26,7 @@ if (!isset($_SESSION['admin_logged_in'])) {
     <head>
         <meta charset="UTF-8">
         <title>Connexion Administration</title>
-        <link rel="stylesheet" href="style.css">
+        <link rel="stylesheet" href="style.css?v=<?= time() ?>">
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
     </head>
     <body>
@@ -59,7 +59,7 @@ $tire_categories = $pdo->query("SELECT * FROM tire_categories")->fetchAll();
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Tableau de Bord - Adams Pneumatique</title>
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="style.css?v=<?= time() ?>">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
 </head>
 <body>
